705b20f0dba1754dbb6dff9a4711b7370e5b1c21
Apanas9246 <apanas9246@bths.edu>
2020-08-25 03:15:49 -0400

    update json

--- main.py
+++ main.py
@@ -68,7 +68,8 @@ death_images = {"fall": pygame.image.load("death_images/fall.png"),
                 "burn": pygame.image.load("death_images/burn.png"),
                 "witherskeleton": pygame.image.load("death_images/witherskeleton.png"),
                 "void": pygame.image.load("death_images/void.png"),
-                "endermite": pygame.image.load("death_images/endermite.png")}
+                "endermite": pygame.image.load("death_images/endermite.png"),
+                "magmacube": pygame.image.load("death_images/magmacube.png")}
 
 space_per_player = WIDTH / player_qnt
 
